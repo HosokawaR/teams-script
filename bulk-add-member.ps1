c13c840c48095b7b68a1fdca7fa331dcbcf6bf5e
HosokawaR <thin.river.jp@gmail.com>
2022-07-27 14:12:25 +0900

    update readme

--- bulk-add-member.ps1
+++ bulk-add-member.ps1
@@ -3,7 +3,7 @@ $csvPath = "<CSV のパス>"
 
 Connect-MicrosoftTeams
 Import-Csv -Path $csvPath | ForEach-Object {
-    Add-TeamUser -GroupId $groupId -user $_.email;
+    Add-TeamUser -GroupId $groupId -User $_.email;
     Write-Output "Added $($_.email)"; 
     Start-Sleep 10;
 }
